8290887f02b80428d431b00812c9697d96ed6f92
JackEdwardLyons <jack.edward.lyons@gmail.com>
2016-12-28 16:21:44 +1000

    create new bug module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,14 @@
+// import Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BugModule } from '../bug/bug.module';
+// import Components
 import { AppComponent } from './app.component';
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports:      [ BrowserModule, BugModule ],
     declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    bootstrap:    [ AppComponent ]
 })
 
 export class AppModule { }
