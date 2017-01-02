6173ef2c47f07e2b7761285e4a72118021581b4e
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-01-02 14:35:59 +1000

    small update

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app',
     template: `
-    <navbar></navbar>
+    <navbar>
+    </navbar>
         <div class="container">
-            <router-outlet></router-outlet>
+            <router-outlet>
+            </router-outlet>
         </div>
     `,
     styles: [`
