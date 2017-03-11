db435cda510b39b3c98c8838e11c9bd9cd3a18b6
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-03-12 08:38:48 +1000

    playing with FormBuilder component and comparing syntax when creating form groups, not much variation between the two

--- app/bug/bug-detail/bug-detail.component.js
+++ app/bug/bug-detail/bug-detail.component.js
@@ -10,26 +10,41 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var forms_1 = require('@angular/forms');
+var forbidden_string_validator_1 = require('../../shared/validation/forbidden-string.validator');
 var BugDetailComponent = (function () {
-    function BugDetailComponent() {
+    // Inject the FormBuilder module
+    function BugDetailComponent(formB) {
+        this.formB = formB;
         this.modalId = "bugModal";
     }
     BugDetailComponent.prototype.ngOnInit = function () {
         this.configureForm();
     };
     BugDetailComponent.prototype.configureForm = function () {
-        // this is an example of Reactive forms 
-        // which create specific fields within
-        // the Bug description form.
-        this.bugForm = new forms_1.FormGroup({
+        /* Demonstrating two methods to creating Reactive Forms
+         * This is an example of Reactive forms
+         * which create specific fields within
+         * the Bug description form.
+         */
+        /* Method 1: Using imported FormBuilder Component
+         *************************************************/
+        this.bugForm = this.formB.group({
+            title: [null, [forms_1.Validators.required, forbidden_string_validator_1.forbiddenStringValidator(/puppy/i)]],
+            status: [1, forms_1.Validators.required],
+            severity: [1, forms_1.Validators.required],
+            description: [null, forms_1.Validators.required]
+        });
+        /* Method 2: Creating a new FormGroup and FormControl Object
+         ************************************************************
+        this.bugForm = new FormGroup({
             // set default values in FormControl with the
             // first parameter, and validation in the second
-            title: new forms_1.FormControl(null, forms_1.Validators.required),
+            title: new FormControl(null, [Validators.required, forbiddenStringValidator(/puppy/i)]),
             // select initial as first value
-            status: new forms_1.FormControl(1, forms_1.Validators.required),
-            severity: new forms_1.FormControl(1, forms_1.Validators.required),
-            description: new forms_1.FormControl(null, forms_1.Validators.required)
-        });
+            status: new FormControl(1, Validators.required),
+            severity: new FormControl(1, Validators.required),
+            description: new FormControl(null, Validators.required)
+        }); */
     };
     BugDetailComponent.prototype.submitForm = function () {
         console.log(this.bugForm); // in the console look at the _value property
@@ -41,7 +56,7 @@ var BugDetailComponent = (function () {
             templateUrl: 'bug-detail.component.html',
             styleUrls: ['bug-detail.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [forms_1.FormBuilder])
     ], BugDetailComponent);
     return BugDetailComponent;
 }());
