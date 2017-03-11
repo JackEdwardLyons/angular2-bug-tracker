cb381a830860b892238345e47b84fc2699e330a4
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-03-11 17:03:04 +1000

    add validation styles

--- app/bug/bug-detail/bug-detail.component.ts
+++ app/bug/bug-detail/bug-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ForbiddenStringValidator } from '../../shared/validation/forbidden-string.validator';
 
 @Component({
     moduleId: module.id,
